972ead9b46161064dbcb9d08e1109569044d3314
leevi raudel <leeviraudel.business@gmail.com>
2017-11-09 18:59:54 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var routes = require('./routes');
+app = express();
+var port = '3000';
+
+// MORO
+
+app.set('view engine', 'ejs');
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.home);
+
+app.get('*', routes.notFound);
+
+app.listen(port, function() {
+	console.log("Running on port " + port);
+});
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var package_fav = require('../package.json');
+
+
+
+exports.home = function (req, res) {
+	res.render('home', {
+		titulo: 'Smath Labs School App | Watson @ School',
+		favicon: package_fav.favicon,
+	});
+};
+
+
+
+exports.notFound = function(req,res) {
+	res.render('error_page', {
+		title: 'Title goes here',
+	});
+};
