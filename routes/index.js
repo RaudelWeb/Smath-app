a68e31db99131a0c855bac3eb571677a48fa0a02
leevi raudel <leeviraudel.business@gmail.com>
2017-11-10 22:38:21 +0200

    n

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 var package_fav = require('../package.json');
-	
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+	dd = '0'+dd
+}
+
+if(mm<10) {
+	mm = '0'+mm
+}
+
+today = dd + '/' + mm + '/' + yyyy;
+
 
 exports.home = function (req, res) {
 	res.render('home', {
 		titulo: 'Smath Labs School App | Watson @ School',
 		favicon: package_fav.favicon,
+		profile_img: package_fav.profile,
+		date : today,
 	});
+
 };
 
 
